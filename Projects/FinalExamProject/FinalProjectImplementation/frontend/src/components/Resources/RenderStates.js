5a0ab095b5138363fc99d0adc643574582810c07
lroyevans <99619477+lroyevans@users.noreply.github.com>
2026-07-29 23:54:30 -0400

    Delete Projects directory

--- Projects/FinalExamProject/FinalProjectImplementation/frontend/src/components/Resources/RenderStates.js
+++
@@ -1,8 +0,0 @@
-const renderState = Object.freeze({
-    BOOK: "book",
-    DELETE: "del",
-    ADD: "add",
-    UPDATE: "up"
-});
-
-export default renderState;
--- Projects/Project1/Portfolio_Part2/README.md
+++
@@ -1,1 +0,0 @@
-# CMSC-335-Portfolio-Website
